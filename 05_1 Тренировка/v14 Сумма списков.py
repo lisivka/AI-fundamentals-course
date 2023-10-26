0faabbbe1c120820a6bfa9107b5b130992a10641
lisivka <lisivka@gmail.com>
2023-10-27 00:33:27 +0300

    add простий персетрон

---
+++ 05_1 Тренировка/v14 Сумма списков.py
@@ -0,0 +1,16 @@
+list1 = [0, 1]
+list2 = [1, 2, 3]
+
+# Проверка, какой список короче, и использование его длины для итерации
+# if len(list1) < len(list2):
+#     result = sum(list1[i] * list2[i] for i in range(len(list1)))
+# else:
+#     result = sum(list1[i] * list2[i] for i in range(len(list2)))
+
+# print("Результат умножения и суммирования:", result)
+summa = 0
+for i in range(len(list1)):
+    for j in range(len(list2)):
+        print(list1[i], list2[j], list1[i] * list2[j] )
+        summa += list1[i] * list2[j]
+print("Результат умножения и суммирования:", summa)
