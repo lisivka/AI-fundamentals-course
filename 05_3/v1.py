d51090799bea99cee07fb20bad30d4359b146c95
lisivka <lisivka@gmail.com>
2023-11-04 22:45:55 +0200

    v17 for 5_1!!

--- 05_3/v1.py
+++ 05_3/v1.py
@@ -5,13 +5,34 @@ from keras.optimizers import SGD
 import tensorflow as tf
 
 def create_neural_network(input_dim, hidden_units):
+    """
+    Create a neural network model with a specified number of hidden units.
+
+    Args:
+        input_dim (int): Number of input features.
+        hidden_units (int): Number of units in the hidden layer.
+
+    Returns:
+        model (Sequential): Compiled neural network model.
+    """
+
     model = Sequential()
-    model.add(Dense(hidden_units, input_dim=input_dim, activation='relu'))
+    model.add(Dense(hidden_units, input_dim=input_dim, activation='sigmoid'))  # Hidden layer with 'hidden_units' neurons and 'input_dim' input features
     model.add(Dense(1, activation='linear'))  # Output layer for regression tasks
-    model.compile(optimizer='adam', loss='mean_squared_error')  # You can change the optimizer and loss function as needed
+    model.compile(optimizer='adam', loss='mean_squared_error', )  # You can change the optimizer and loss function as needed
     return model
 
 def train_neural_network(model, X_train, y_train, num_epochs, batch_size):
+    """
+    Train the neural network model on the training data.
+
+    Args:
+        model (Sequential): Compiled neural network model.
+        X_train (ndarray): Training input features.
+        y_train (ndarray): Training labels.
+        num_epochs (int): Number of training epochs.
+        batch_size (int): Batch size for training.
+    """
     history = model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, verbose=0)
     return history
 
@@ -21,10 +42,13 @@ y_train = np.array([0, 1, 1, 0])
 
 # Hyperparameters to experiment with
 input_dim = X_train.shape[1]
-learning_rates = [0.001, 0.01, 0.1]
-hidden_units_list = [4, 8, 16]
-num_epochs = 100
-batch_size = 2
+# learning_rates = [0.001, 0.01, 0.1]
+# hidden_units_list = [4, 8, 16]
+learning_rates = [0.1]
+hidden_units_list = [3]
+
+num_epochs = 10000
+batch_size = 4
 
 # Perform hyperparameter tuning
 for learning_rate in learning_rates:
@@ -37,5 +61,5 @@ for learning_rate in learning_rates:
         print("\n", "*" * 50)
         print(f"Learning Rate: {learning_rate}, Hidden Units: {hidden_units}, Loss: {loss}")
         for i in X_train:
-            predictions = model.predict(np.array([i]))
+            predictions = model.predict(np.array([i]), verbose=0)
             print(f"{i} predictions : {predictions}")
